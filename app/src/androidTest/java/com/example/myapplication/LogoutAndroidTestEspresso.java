dc7bde8ed0095fb3ddf9eea2ef6917219a46c963
Gerardo Valenzuela <gjvalenz@usc.edu>
2022-11-21 01:28:37 -0800

    fixed up login, added map display tests, and register tests as well as logout tests

--- app/src/androidTest/java/com/example/myapplication/LogoutAndroidTestEspresso.java
+++ app/src/androidTest/java/com/example/myapplication/LogoutAndroidTestEspresso.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 
+import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Rule;
@@ -15,6 +16,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import androidx.core.content.ContextCompat;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -27,6 +29,7 @@ import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.getIntents;
+import static androidx.test.espresso.intent.Intents.intending;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -43,10 +46,9 @@ import java.util.List;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class LoginActivityAndroidTestEspresso
-{
-    @Rule public ActivityScenarioRule<LoginActivity> activityScenarioRule
-            = new ActivityScenarioRule<>(LoginActivity.class);
+public class LogoutAndroidTestEspresso {
+    @Rule public ActivityScenarioRule<LoginActivity> activityScenarioRule = new ActivityScenarioRule<LoginActivity>(LoginActivity.class);
+
     private Activity getCurrentActivity() {
         final Activity[] a = new Activity[1];
         activityScenarioRule.getScenario().onActivity(activity -> {
@@ -57,16 +59,8 @@ public class LoginActivityAndroidTestEspresso
 
     private View decorView;
 
-    @Before
-    public void setUp() {
-        activityScenarioRule.getScenario().onActivity(activity ->
-            {
-                decorView = activity.getWindow().getDecorView();
-            });
-    }
-
-    @Test
-    public void Test_Login() {
+    private void login()
+    {
         Drawable notAnimatedDrawable = ContextCompat.getDrawable(getCurrentActivity(), com.google.firebase.database.collection.R.drawable.common_google_signin_btn_text_light_normal_background);
         ((ProgressBar) getCurrentActivity().findViewById(R.id.progressBar)).setIndeterminateDrawable(notAnimatedDrawable);
         onView(withId(R.id.email))
@@ -91,28 +85,21 @@ public class LoginActivityAndroidTestEspresso
         Intents.release();
     }
 
-    @Test
-    public void Incorrect_Login() {
-        Drawable notAnimatedDrawable = ContextCompat.getDrawable(getCurrentActivity(), com.google.firebase.database.collection.R.drawable.common_google_signin_btn_text_light_normal_background);
-        ((ProgressBar) getCurrentActivity().findViewById(R.id.progressBar)).setIndeterminateDrawable(notAnimatedDrawable);
-        onView(withId(R.id.email))
-                .perform(typeText("g@f.com"), closeSoftKeyboard());
-        onView(withId(R.id.password)) // wrong password
-                .perform(typeText("123457"), closeSoftKeyboard());
-        onView(withId(R.id.email)).check(matches(withText("g@f.com")));
-        onView(withId(R.id.signIn)).perform(click());
-        Boolean done = false;
-        while(!done)
+    @Before
+    public void setUp() {
+        activityScenarioRule.getScenario().onActivity(activity ->
         {
-            ProgressBar PB = (ProgressBar) getCurrentActivity().findViewById(R.id.progressBar);
-            Log.i("Test", String.valueOf(PB.getVisibility()));
-            if(PB.getVisibility() == View.GONE)
-            {
-                done = true;
-            }
-        }
-        // no intents
-        // instead check toast appears
-        onView(withText(R.string.login_failed_toast_text)).inRoot(withDecorView(Matchers.not(decorView)));
+            decorView = activity.getWindow().getDecorView();
+        });
+    }
+
+    @Test
+    public void Try_Logout() {
+        login();
+        onView(withId(R.id.profilePage))
+                .perform(click());
+        onView(withId(R.id.signOut)).perform(click());
+        Intents.init();
+        intending(hasComponent(LoginActivity.class.getName()));
     }
 }
